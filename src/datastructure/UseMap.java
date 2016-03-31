84190292c63927fc34ad86fcca1bb9e725765e07
Syeda Ummezahra <ummezahra.syed@gmail.com>
2016-03-31 11:50:58 -0400

    Project

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -13,15 +13,17 @@ public class UseMap {
         map.put("Australia","Sydney");
         map.put("Canada","Montreal");
         map.put("Bangladesh","Dhaka");
+
+        System.out.println("Name of the country and their citis : "+map.size());
+
         Iterator it = map.entrySet().iterator();// enter to iterator through map.entrySet
         while(it.hasNext()){
            System.out.println(it.next());
 
-
-
         //for(Map.Entry<String,String> cursor:map.entrySet()) {
         //System.out.println("Key: " + cursor.getKey()+"  " + "Value: "+cursor.getValue());
-        }
+       // }
+    }
         }
 
 
